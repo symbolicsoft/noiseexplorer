1208771027634c85bafdc7d2bae47e4919f1c179
Nadim Kobeissi <nadim@symbolic.software>
2018-05-24 16:42:42 +0200

    Parser now checks for DH with inexistent keys and unused key shares (suggested by Katriel Cohn-Gordon.)

--- src/parser/noiseParser.js
+++ src/parser/noiseParser.js
@@ -154,11 +154,6 @@ function peg$parse(input, options) {
       		check.messages(pattern);
       		check.psk(pattern);
       		check.tokenlessMessages(pattern);
-      		if ((g.s > 1) || (g.e > 1) ||
-      			(g.rs > 1) || (g.re > 1)
-      		) {
-      			error(errMsg.keySentMoreThanOnce);
-      		}
           	return pattern;
           },
       peg$c3 = /^[a-zA-Z0-9]/,
@@ -194,12 +189,12 @@ function peg$parse(input, options) {
       peg$c19 = peg$literalExpectation("psk", false),
       peg$c20 = "rs",
       peg$c21 = peg$literalExpectation("rs", false),
-      peg$c22 = "es",
-      peg$c23 = peg$literalExpectation("es", false),
+      peg$c22 = "ss",
+      peg$c23 = peg$literalExpectation("ss", false),
       peg$c24 = "se",
       peg$c25 = peg$literalExpectation("se", false),
-      peg$c26 = "ss",
-      peg$c27 = peg$literalExpectation("ss", false),
+      peg$c26 = "es",
+      peg$c27 = peg$literalExpectation("es", false),
       peg$c28 = "ee",
       peg$c29 = peg$literalExpectation("ee", false),
       peg$c30 = "s",
@@ -974,150 +969,172 @@ function peg$parse(input, options) {
   }
 
 
-  	const g = {
-  		s: 0,
-  		e: 0,
-  		rs: 0,
-  		re: 0
-  	};
+  const g = {
+  	s: 0,
+  	e: 0,
+  	rs: 0,
+  	re: 0,
+  	ss: 0,
+  	se: 0,
+  	es: 0,
+  	ee: 0
+  };
 
-  	const util = {
-  		hasDuplicates: (arr) => {
-  			let vo = {};
-  			for (let i = 0; i < arr.length; ++i) {
-  				let v = arr[i];
-  				if (v in vo) { return true; }
-  				vo[v] = true;
+  const util = {
+  	hasDuplicates: (arr) => {
+  		let vo = {};
+  		for (let i = 0; i < arr.length; ++i) {
+  			let v = arr[i];
+  			if (v in vo) { return true; }
+  			vo[v] = true;
+  		}
+  		return false;
+  	}
+  };
+
+  // Ideas for more checks:
+  // - Check if pattern names are accurate.
+  const errMsg = {
+  	tooLongName: 'Noise Handshake Pattern names with a maximum length of 16 characters are currently supported.',
+  	tooManyTokens: 'Noise Message Patterns with a maximum of 8 tokens are currently supported.',
+  	tooManyMessages: 'Noise Handshake Patterns with a maximum of 8 message patterns are currently supported.',
+  	dupTokens: 'Noise Handshake Patterns must not contain duplicate tokens in the same message flight.',
+  	keySentMoreThanOnce: 'Principals must not send their static public key or ephemeral public key more than once per handshake.',
+  	wrongPskModifier: 'PSK modifiers must correctly indicate the position of PSK tokens.',
+  	wrongPskLocation: 'PSK tokens must appear at the beginning or end of the first handshake message or at the end of any other handshake message.',
+  	moreThanOnePsk: 'Noise Handshake Patterns with a maximum of one PSK are currently supported.',
+  	pskNotAtEndOfName: 'PSK modifiers must appear at the end of the Noise Handshake Pattern name.',
+  	wrongMessageDir: 'Message direction within a Noise Handshake Pattern must alternate (initiator -> responder, initiator <- responder), with the first message being sent by the initiator.',
+  	tokenlessNotLast: 'Noise Handshake Patterns can only contain tokenless handshake messages at the very bottom of the pattern.',
+  	dhWithUnknownKey: 'Principals cannot perform a Diffie-Hellman operation with a key share that does not exist.',
+  	unusedKeySent: 'Noise Handshake Patterns should not contain key shares that are not subsequently used in any Diffie-Hellman operation.'
+  };
+
+  const check = {
+  	preMessages: (pattern) => {
+  		pattern.preMessages.forEach((preMessage) => {
+  			if (preMessage.tokens.indexOf('s') >= 0) {
+  				(preMessage.dir === 'send')? g.s++ : g.rs++;
   			}
+  			if (preMessage.tokens.indexOf('e') >= 0) {
+  				(preMessage.dir === 'send')? g.e++ : g.re++;
+  			}
+  		});
+  	},
+  	messages: (pattern) => {
+  		if (pattern.messages.length > 8) {
+  			error(errMsg.tooManyMessages);
+  		}
+  		pattern.messages.forEach((message, i) => {
+  			if (
+  				((i % 2)  && (message.dir === 'send')) ||
+  				(!(i % 2) && (message.dir === 'recv'))
+  			) {
+  				error(errMsg.wrongMessageDir);
+  			}
+  			if (util.hasDuplicates(message.tokens)) {
+  				error(errMsg.dupTokens);
+  			}
+  			if (message.tokens.length > 8) {
+  				error(errMsg.tooManyTokens);
+  			}
+  			if (message.tokens.indexOf('s') >= 0) {
+  				(message.dir === 'send')? g.s++ : g.rs++;
+  			}
+  			if (message.tokens.indexOf('e') >= 0) {
+  				(message.dir === 'send')? g.e++ : g.re++;
+  			}
+  			if (message.tokens.indexOf('ss') >= 0) {
+  				g.ss++;
+  			}
+  			if (message.tokens.indexOf('se') >= 0) {
+  				g.se++;
+  			}
+  			if (message.tokens.indexOf('es') >= 0) {
+  				g.es++;
+  			}
+  			if (message.tokens.indexOf('ee') >= 0) {
+  				g.ee++;
+  			}
+  			if (
+  				(g.ss && (!g.s || !g.rs)) ||
+  				(g.se && (!g.s || !g.re)) ||
+  				(g.es && (!g.e || !g.rs)) ||
+  				(g.ee && (!g.e || !g.re))
+  		 	) {
+  				error(errMsg.dhWithUnknownKey);
+  			}
+  		});
+  		if  ((g.s > 1) || (g.e > 1) ||
+  			(g.rs > 1) || (g.re > 1)
+  		) {
+  			error(errMsg.keySentMoreThanOnce);
+  		}
+  		if (
+  			(g.s  && (!g.ss && !g.se)) ||
+  			(g.e  && (!g.es && !g.ee)) ||
+  			(g.rs && (!g.ss && !g.es)) ||
+  			(g.re && (!g.se && !g.ee))
+  		) {
+  			error(errMsg.unusedKeySent);
+  		}
+  	},
+  	psk: (pattern) => {
+  		let pskMods = pattern.name.match(/psk\d/g);
+  		if (!pskMods) {
+  			pattern.messages.forEach((message) => {
+  				if (message.tokens.indexOf('psk') >= 0) {
+  					error(errMsg.wrongPskModifier);
+  				}
+  			});
   			return false;
   		}
-  	};
-
-  	const check = {
-  		preMessages: (pattern) => {
-  			pattern.preMessages.forEach((preMessage) => {
-  				if (preMessage.tokens.indexOf('s') >= 0) {
-  					if (preMessage.dir === 'send') {
-  						g.s  = g.s + 1;
-  					} else {
-  						g.rs = g.rs + 1;
-  					}
-  				}
-  				if (preMessage.tokens.indexOf('e') >= 0) {
-  					if (preMessage.dir === 'send') {
-  						g.e  = g.e + 1;
-  					} else {
-  						g.re = g.re + 1;
-  					}
-  				}
-  			});
-  		},
-  		messages: (pattern) => {
-  			if (pattern.messages.length > 8) {
-  				error(errMsg.tooManyMessages);
-  			}
-  			pattern.messages.forEach((message, i) => {
-  				if (
-  					((i % 2)  && (message.dir === 'send')) ||
-  					(!(i % 2) && (message.dir === 'recv'))
-  				) {
-  					error(errMsg.wrongMessageDir);
-  				}
-  				if (util.hasDuplicates(message.tokens)) {
-  					error(errMsg.dupTokens);
-  				}
-  				if (message.tokens.length > 8) {
-  					error(errMsg.tooManyTokens);
-  				}
-  				if (message.tokens.indexOf('s') >= 0) {
-  					if (message.dir === 'send') {
-  						g.s  = g.s + 1;
-  					} else {
-  						g.rs = g.rs + 1;
-  					}
-  				}
-  				if (message.tokens.indexOf('e') >= 0) {
-  					if (message.dir === 'send') {
-  						g.e  = g.e + 1;
-  					} else {
-  						g.re = g.re + 1;
-  					}
-  				}
-  			});
-  		},
-  		psk: (pattern) => {
-  			let pskMods = pattern.name.match(/psk\d/g);
-  			if (pskMods) {
-  				if (pskMods.length > 1) {
-  					error(errMsg.moreThanOnePsk);
-  				}
-  				if (!/psk\d$/.test(pattern.name)) {
-  					error(errMsg.pskNotAtEndOfName);
-  				}
-  				pskMods.forEach((pskMod) => {
-  					pskMod = parseInt(pskMod.charAt(3), 10);
-  					if (pskMod > pattern.messages.length) {
-  						error(errMsg.wrongPskModifier);
-  					} else if (pskMod === 0) {
-  						let tokens = pattern.messages[pskMod].tokens;
-  						if (tokens.indexOf('psk') < 0) {
-  							error(errMsg.wrongPskModifier);
-  						} else if (tokens.indexOf('psk') > 0) {
-  							error(errMsg.wrongPskLocation);
-  						}
-  					} else {
-  						let tokens = pattern.messages[pskMod - 1].tokens;
-  						if (tokens.indexOf('psk') < 0) {
-  							error(errMsg.wrongPskModifier);
-  						} else if (tokens.indexOf('psk') !== (tokens.length - 1)) {
-  							(pskMod === 1)? error(errMsg.wrongPskModifier) : error(errMsg.wrongPskLocation);
-  						}
-  					}
-  				});
-  			} else {
-  				pattern.messages.forEach((message) => {
-  					if (message.tokens.indexOf('psk') > 0) {
-  						error(errMsg.wrongPskModifier);
-  					}
-  				});
-  			}
-  		},
-  		tokenlessMessages: (pattern) => {
-  			let tokenlessMessage = -1;
-  			pattern.messages.forEach((message, i) => {
-  				if (
-  					(message.tokens.length === 0) &&
-  					(tokenlessMessage === -1)
-  				) {
-  					tokenlessMessage = i;
-  				}
-  				if (
-  					(message.tokens.length > 0) &&
-  					(tokenlessMessage >= 0) &&
-  					(i > tokenlessMessage)
-  				) {
-  					error(errMsg.tokenlessNotLast);
-  				}
-  			});
+  		if (pskMods.length > 1) {
+  			error(errMsg.moreThanOnePsk);
   		}
-  	};
-
-  	// Ideas for more checks:
-  	// - Check if all sent ephemerals are actually used (suggested by Katriel.)
-  	// - Check if pattern names are accurate.
-  	const errMsg = {
-  		tooLongName: 'Handshake pattern names with a maximum length of 16 characters are currently supported.',
-  		tooManyTokens: 'Message patterns with a maximum of 8 tokens are currently supported.',
-  		tooManyMessages: 'Handshake patterns with a maximum of 8 message patterns are currently supported.',
-  		dupTokens: 'Noise pattern must not contain duplicate tokens in the same message flight.',
-  		keySentMoreThanOnce: 'Parties must not send their static public key or ephemeral public key more than once per handshake.',
-  		wrongPskModifier: 'PSK modifiers must correctly indicate the position of PSK tokens.',
-  		wrongPskLocation: 'PSK tokens must appear at the beginning or end of the first handshake message or at the end of any other handshake message.',
-  		moreThanOnePsk: 'Handshake patterns with a maximum of one PSK are currently supported.',
-  		pskNotAtEndOfName: 'PSK modifiers must appear at the end of the Noise handshake pattern name.',
-  		wrongMessageDir: 'Message patterns must alternate (initiator -> responder, initiator <- responder), with the first message being sent by the initiator.',
-  		tokenlessNotLast: 'Noise handshake patterns can only contain tokenless handshake messages at the very bottom of the pattern.'
-  	};
+  		if (!/psk\d$/.test(pattern.name)) {
+  			error(errMsg.pskNotAtEndOfName);
+  		}
+  		pskMods.forEach((pskMod) => {
+  			pskMod = parseInt(pskMod.charAt(3), 10);
+  			if (pskMod > pattern.messages.length) {
+  				error(errMsg.wrongPskModifier);
+  			} else if (pskMod === 0) {
+  				let tokens = pattern.messages[pskMod].tokens;
+  				if (tokens.indexOf('psk') < 0) {
+  					error(errMsg.wrongPskModifier);
+  				} else if (tokens.indexOf('psk') > 0) {
+  					error(errMsg.wrongPskLocation);
+  				}
+  			} else {
+  				let tokens = pattern.messages[pskMod - 1].tokens;
+  				if (tokens.indexOf('psk') < 0) {
+  					error(errMsg.wrongPskModifier);
+  				} else if (tokens.indexOf('psk') !== (tokens.length - 1)) {
+  					(pskMod === 1)? error(errMsg.wrongPskModifier) : error(errMsg.wrongPskLocation);
+  				}
+  			}
+  		});
+  	},
+  	tokenlessMessages: (pattern) => {
+  		let tokenlessMessage = -1;
+  		pattern.messages.forEach((message, i) => {
+  			if (
+  				(message.tokens.length === 0) &&
+  				(tokenlessMessage === -1)
+  			) {
+  				tokenlessMessage = i;
+  			}
+  			if (
+  				(message.tokens.length > 0) &&
+  				(tokenlessMessage >= 0) &&
+  				(i > tokenlessMessage)
+  			) {
+  				error(errMsg.tokenlessNotLast);
+  			}
+  		});
+  	}
+  };
 
 
   peg$result = peg$startRuleFunction();
--- src/parser/noiseParser.pegjs
+++ src/parser/noiseParser.pegjs
@@ -3,7 +3,11 @@ const g = {
 	s: 0,
 	e: 0,
 	rs: 0,
-	re: 0
+	re: 0,
+	ss: 0,
+	se: 0,
+	es: 0,
+	ee: 0
 };
 
 const util = {
@@ -18,22 +22,32 @@ const util = {
 	}
 };
 
+// Ideas for more checks:
+// - Check if pattern names are accurate.
+const errMsg = {
+	tooLongName: 'Noise Handshake Pattern names with a maximum length of 16 characters are currently supported.',
+	tooManyTokens: 'Noise Message Patterns with a maximum of 8 tokens are currently supported.',
+	tooManyMessages: 'Noise Handshake Patterns with a maximum of 8 message patterns are currently supported.',
+	dupTokens: 'Noise Handshake Patterns must not contain duplicate tokens in the same message flight.',
+	keySentMoreThanOnce: 'Principals must not send their static public key or ephemeral public key more than once per handshake.',
+	wrongPskModifier: 'PSK modifiers must correctly indicate the position of PSK tokens.',
+	wrongPskLocation: 'PSK tokens must appear at the beginning or end of the first handshake message or at the end of any other handshake message.',
+	moreThanOnePsk: 'Noise Handshake Patterns with a maximum of one PSK are currently supported.',
+	pskNotAtEndOfName: 'PSK modifiers must appear at the end of the Noise Handshake Pattern name.',
+	wrongMessageDir: 'Message direction within a Noise Handshake Pattern must alternate (initiator -> responder, initiator <- responder), with the first message being sent by the initiator.',
+	tokenlessNotLast: 'Noise Handshake Patterns can only contain tokenless handshake messages at the very bottom of the pattern.',
+	dhWithUnknownKey: 'Principals cannot perform a Diffie-Hellman operation with a key share that does not exist.',
+	unusedKeySent: 'Noise Handshake Patterns should not contain key shares that are not subsequently used in any Diffie-Hellman operation.'
+};
+
 const check = {
 	preMessages: (pattern) => {
 		pattern.preMessages.forEach((preMessage) => {
 			if (preMessage.tokens.indexOf('s') >= 0) {
-				if (preMessage.dir === 'send') {
-					g.s  = g.s + 1;
-				} else {
-					g.rs = g.rs + 1;
-				}
+				(preMessage.dir === 'send')? g.s++ : g.rs++;
 			}
 			if (preMessage.tokens.indexOf('e') >= 0) {
-				if (preMessage.dir === 'send') {
-					g.e  = g.e + 1;
-				} else {
-					g.re = g.re + 1;
-				}
+				(preMessage.dir === 'send')? g.e++ : g.re++;
 			}
 		});
 	},
@@ -55,57 +69,82 @@ const check = {
 				error(errMsg.tooManyTokens);
 			}
 			if (message.tokens.indexOf('s') >= 0) {
-				if (message.dir === 'send') {
-					g.s  = g.s + 1;
-				} else {
-					g.rs = g.rs + 1;
-				}
+				(message.dir === 'send')? g.s++ : g.rs++;
 			}
 			if (message.tokens.indexOf('e') >= 0) {
-				if (message.dir === 'send') {
-					g.e  = g.e + 1;
-				} else {
-					g.re = g.re + 1;
-				}
+				(message.dir === 'send')? g.e++ : g.re++;
+			}
+			if (message.tokens.indexOf('ss') >= 0) {
+				g.ss++;
+			}
+			if (message.tokens.indexOf('se') >= 0) {
+				g.se++;
+			}
+			if (message.tokens.indexOf('es') >= 0) {
+				g.es++;
+			}
+			if (message.tokens.indexOf('ee') >= 0) {
+				g.ee++;
+			}
+			if (
+				(g.ss && (!g.s || !g.rs)) ||
+				(g.se && (!g.s || !g.re)) ||
+				(g.es && (!g.e || !g.rs)) ||
+				(g.ee && (!g.e || !g.re))
+		 	) {
+				error(errMsg.dhWithUnknownKey);
 			}
 		});
+		if  ((g.s > 1) || (g.e > 1) ||
+			(g.rs > 1) || (g.re > 1)
+		) {
+			error(errMsg.keySentMoreThanOnce);
+		}
+		if (
+			(g.s  && (!g.ss && !g.se)) ||
+			(g.e  && (!g.es && !g.ee)) ||
+			(g.rs && (!g.ss && !g.es)) ||
+			(g.re && (!g.se && !g.ee))
+		) {
+			error(errMsg.unusedKeySent);
+		}
 	},
 	psk: (pattern) => {
 		let pskMods = pattern.name.match(/psk\d/g);
-		if (pskMods) {
-			if (pskMods.length > 1) {
-				error(errMsg.moreThanOnePsk);
-			}
-			if (!/psk\d$/.test(pattern.name)) {
-				error(errMsg.pskNotAtEndOfName);
-			}
-			pskMods.forEach((pskMod) => {
-				pskMod = parseInt(pskMod.charAt(3), 10);
-				if (pskMod > pattern.messages.length) {
-					error(errMsg.wrongPskModifier);
-				} else if (pskMod === 0) {
-					let tokens = pattern.messages[pskMod].tokens;
-					if (tokens.indexOf('psk') < 0) {
-						error(errMsg.wrongPskModifier);
-					} else if (tokens.indexOf('psk') > 0) {
-						error(errMsg.wrongPskLocation);
-					}
-				} else {
-					let tokens = pattern.messages[pskMod - 1].tokens;
-					if (tokens.indexOf('psk') < 0) {
-						error(errMsg.wrongPskModifier);
-					} else if (tokens.indexOf('psk') !== (tokens.length - 1)) {
-						(pskMod === 1)? error(errMsg.wrongPskModifier) : error(errMsg.wrongPskLocation);
-					}
-				}
-			});
-		} else {
+		if (!pskMods) {
 			pattern.messages.forEach((message) => {
-				if (message.tokens.indexOf('psk') > 0) {
+				if (message.tokens.indexOf('psk') >= 0) {
 					error(errMsg.wrongPskModifier);
 				}
 			});
+			return false;
 		}
+		if (pskMods.length > 1) {
+			error(errMsg.moreThanOnePsk);
+		}
+		if (!/psk\d$/.test(pattern.name)) {
+			error(errMsg.pskNotAtEndOfName);
+		}
+		pskMods.forEach((pskMod) => {
+			pskMod = parseInt(pskMod.charAt(3), 10);
+			if (pskMod > pattern.messages.length) {
+				error(errMsg.wrongPskModifier);
+			} else if (pskMod === 0) {
+				let tokens = pattern.messages[pskMod].tokens;
+				if (tokens.indexOf('psk') < 0) {
+					error(errMsg.wrongPskModifier);
+				} else if (tokens.indexOf('psk') > 0) {
+					error(errMsg.wrongPskLocation);
+				}
+			} else {
+				let tokens = pattern.messages[pskMod - 1].tokens;
+				if (tokens.indexOf('psk') < 0) {
+					error(errMsg.wrongPskModifier);
+				} else if (tokens.indexOf('psk') !== (tokens.length - 1)) {
+					(pskMod === 1)? error(errMsg.wrongPskModifier) : error(errMsg.wrongPskLocation);
+				}
+			}
+		});
 	},
 	tokenlessMessages: (pattern) => {
 		let tokenlessMessage = -1;
@@ -126,23 +165,6 @@ const check = {
 		});
 	}
 };
-
-// Ideas for more checks:
-// - Check if all sent ephemerals are actually used (suggested by Katriel.)
-// - Check if pattern names are accurate.
-const errMsg = {
-	tooLongName: 'Handshake pattern names with a maximum length of 16 characters are currently supported.',
-	tooManyTokens: 'Message patterns with a maximum of 8 tokens are currently supported.',
-	tooManyMessages: 'Handshake patterns with a maximum of 8 message patterns are currently supported.',
-	dupTokens: 'Noise pattern must not contain duplicate tokens in the same message flight.',
-	keySentMoreThanOnce: 'Parties must not send their static public key or ephemeral public key more than once per handshake.',
-	wrongPskModifier: 'PSK modifiers must correctly indicate the position of PSK tokens.',
-	wrongPskLocation: 'PSK tokens must appear at the beginning or end of the first handshake message or at the end of any other handshake message.',
-	moreThanOnePsk: 'Handshake patterns with a maximum of one PSK are currently supported.',
-	pskNotAtEndOfName: 'PSK modifiers must appear at the end of the Noise handshake pattern name.',
-	wrongMessageDir: 'Message patterns must alternate (initiator -> responder, initiator <- responder), with the first message being sent by the initiator.',
-	tokenlessNotLast: 'Noise handshake patterns can only contain tokenless handshake messages at the very bottom of the pattern.'
-};
 }
 
 Pattern =
@@ -159,11 +181,6 @@ Pattern =
 		check.messages(pattern);
 		check.psk(pattern);
 		check.tokenlessMessages(pattern);
-		if ((g.s > 1) || (g.e > 1) ||
-			(g.rs > 1) || (g.re > 1)
-		) {
-			error(errMsg.keySentMoreThanOnce);
-		}
     	return pattern;
     }
 
@@ -195,7 +212,7 @@ Arrow =
 	}
 
 Token =
-	('psk' / 'rs' / 'es' / 'se' / 'ss' / 'ee' / 's' / 'e') {
+	('psk' / 'rs' / 'ss' / 'se' / 'es' / 'ee' / 's' / 'e') {
 		return text();
 	}
 
