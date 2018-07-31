b8f305e3bbd1579ab929ae78282d06aba645bb7d
Nadim Kobeissi <nadim@symbolic.software>
2018-07-31 09:52:44 +0200

    Increase offset.

--- src/parser/noiseReader.js
+++ src/parser/noiseReader.js
@@ -360,7 +360,7 @@ const render = (
 	let arrowSvg = [];
 	let analysisTxt = [];
 	let offset = 30;
-	let offsetIncrement = 155;
+	let offsetIncrement = 165;
 	if (pattern.preMessages.length) {
 		pattern.preMessages.forEach((preMessage) => {
 			arrowSvg.push(htmlTemplates[`${preMessage.dir}PreMessage`](
