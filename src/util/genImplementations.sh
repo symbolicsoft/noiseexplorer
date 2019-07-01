c0724a87dd581e33e83b4ea9fa11212d341b1920
Georgio Nicolas <me@georgio.xyz>
2019-07-01 11:55:23 +0300

    WIP #4: Updated the Noise Explorer engine to support generating WASM implementations.
    # Changelog:
    
    1. ### Noise Explorer CLI:
    	- Added `wasm` as a valid value for the `—generate` flag.
    	- Added full support for generating WASM implementations and 			tests.0
    2. ### src/MakeFile
    	- `make dependencies` installs [wasm-pack]() as it is a required dev dependency for generating and testing WASM implementations.
    
    	- `make implementations`  generates custom Rust code that compiles to WASM, in addition to all the implementations it supported earlier.
    
    	- `make wasm` compiles WASM binaries and generates helper files. It is recommended to run `make implementations` before executing `make wasm` for fresh implementations.
    
    	- `make tests`  runs tests for WASM implementations in addition to all the other tests supported earlier. It is recommended that you would have Google Chrome installed on your computer, as the WASM tests make use of its engine in headless mode.

--- src/util/genImplementations.sh
+++ src/util/genImplementations.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 echo "[NoiseExplorer] Generating implementations..."
-cd ..
+cd ../
 for pattern in ../patterns/*.noise; do
 	node noiseExplorer \
 		--generate=go --pattern=$pattern
 	node noiseExplorer \
 		--generate=rs --pattern=$pattern
+	node noiseExplorer \
+		--generate=wasm --pattern=$pattern
 done
+
 cd ../implementations/go
 go get -d ./...
-cd ../../src/util
